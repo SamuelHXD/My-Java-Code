2efeeeff926d21c4384b32e980a411b34881ef71
herong <1358612584@qq.com>
2019-10-28 14:14:48 +0800

    把本地的代码提交到Git

--- BlackHorse/src/prac03/MainThread.java
+++ BlackHorse/src/prac03/MainThread.java
@@ -5,8 +5,8 @@ import java.util.MissingFormatArgumentException;
 public class MainThread extends Thread{
     @Override
     public void run() {
-        for (int i = 0; i < 20; i++) {
-            System.out.println("run:"+i);
-        }
+        Thread thread = Thread.currentThread();
+        System.out.println(thread);
+
     }
 }
